bf94c46532c150779d382322d5599b851ce79347
MushfiqurRahman <mushfique_sust@yahoo.com>
2014-03-11 10:33:15 +0600

    Dynamically input controls and table row has been added to fragment

--- src/com/shaiun/surveysystem/MustHaveSkuFragment.java
+++ src/com/shaiun/surveysystem/MustHaveSkuFragment.java
@@ -1,5 +1,8 @@
 package com.shaiun.surveysystem;
 
+import java.util.ArrayList;
+
+import android.app.ActionBar.LayoutParams;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.renderscript.Sampler;
@@ -7,8 +10,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MustHaveSkuFragment extends Fragment{
 	
@@ -16,7 +23,42 @@ public class MustHaveSkuFragment extends Fragment{
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		
-		ScrollView sv = new ScrollView(inflater.getContext());
+		View v = inflater.inflate(R.layout.must_have_sku, container, false);
+
+		        TableLayout tl = (TableLayout)v.getRootView().findViewById(R.id.tbl_must_have_sku);
+
+        for (int i = 0; i <10; i++)
+        {
+            // Make TR
+            TableRow tr = new TableRow(getActivity());
+            tr.setId(100 + i);
+            //tr.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+
+            // Make TV to hold the details
+            TextView detailstv = new TextView(getActivity());
+            detailstv.setLayoutParams(new TableRow.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f));
+            detailstv.setId(200 + i);
+            detailstv.setText("Hello first");
+            tr.addView(detailstv);
+
+            // Make TV to hold the detailvals
+
+            TextView valstv = new TextView(getActivity());
+            valstv.setId(300 + i);
+            valstv.setText("hello second");
+            tr.addView(valstv);
+            
+            EditText tb = new EditText(getActivity());
+            tb.setId(400 + i);
+            
+            tr.addView(tb);
+
+            tl.addView(tr, new TableLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        }
+		//v.addChildrenForAccessibility(tblRows);
+		return v;
+		
+		/*ScrollView sv = new ScrollView(inflater.getContext());
 		LinearLayout ll = new LinearLayout(inflater.getContext());
 		ll.setOrientation(LinearLayout.VERTICAL);
 		sv.addView(ll);
@@ -27,7 +69,7 @@ public class MustHaveSkuFragment extends Fragment{
 			    ll.addView(cb);
 			    
 		}
-		return sv;	
+		return sv;	*/
 		//return (LinearLayout) inflater.inflate(R.layout.must_have_sku, container, false);
 	}
 	
